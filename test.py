e0a786c26a31b363d0d7ec776f2c59d6b244205c
zzw <2505035375@qq.com>
2024-08-13 22:18:07 +0800

    frist

---
+++ test.py
@@ -0,0 +1,7 @@
+from fightingcv_attention.attention.ECAAttention import ECAAttention
+import torch
+
+input=torch.randn(50,512,7,7)
+eca = ECAAttention(kernel_size=3)
+output=eca(input)
+print(output.shape)
